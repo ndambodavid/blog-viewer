27bbadc5b5d64314bd649d1ff61f80798a07a5fd
ndambodavid <linmik420@gmail.com>
2024-05-10 17:02:47 +0300

    analytics and SEO

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,8 @@ import Footer from '@/components/footer/Footer';
 const Home = ({ featuredPost, posts, snippets, categories, error }) => {
   return (
     <>
-      {/* <SEO /> */}
-      {/* <Analytics /> */}
+      <SEO />
+      <Analytics />
 
       <Navbar />
 
